a47631d5f89860d8e86cc72bea339fe0a806c7eb
prodev-2023 <protech2023.32@gmail.com>
2024-02-18 21:20:50 +0100

    card added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
         primary: "#090B18",
         secondary: "#2DCBD3",
         auxiliary: "#9AD0C2",
-        primaryText: "#0C2D57"
-
+        primaryText: "#0C2D57",
+        decorColor: "#FF6600"
       },
       fontFamily:{
         body: ["Josefin Sans"]
